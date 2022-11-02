9407473b2c041d6ea8fa6307cbbeacc5266828f3
yessense <Akorchemnyj@gmail.com>
2022-11-02 08:37:52 +0300

    added bind

--- src/vsa_encoder/train.py
+++ src/vsa_encoder/train.py
@@ -98,7 +98,7 @@ def train(config):
                          profiler=profiler,
                          callbacks=callbacks,
                          logger=wandb_logger,
-                         check_val_every_n_epoch=5)
+                         check_val_every_n_epoch=5, gradient_clip_val=5.0)
 
     if 'ckpt_path' not in dict_args:
         dict_args['ckpt_path'] = None
