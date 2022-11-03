7694af73b68abfcde3d9df384cd906fad0760ed0
Alexandr Korchemnyi <akorchemnyj@gmail.com>
2022-11-03 12:06:10 +0300

    Added bind_mode parameter

--- src/train.sh
+++ src/train.sh
@@ -1,5 +1,4 @@
 git pull
-source ~/miniconda/etc/profile.d/conda.sh
-conda activate ml_env
-python3 ./vsa_encoder/train.py --batch_size 512 --path_to_dataset "../one_exchange/" --seed 0 --mode "dsprites" --devices 0 --max_epochs 400 --lr 0.00025 --kld_coef 0.001
-
+#source ~/miniconda/etc/profile.d/conda.sh
+#conda activate ml_env
+python3 ./vsa_encoder/train.py --batch_size 512 --path_to_dataset "../one_exchange/" --seed 0 --mode "dsprites" --devices 0 --max_epochs 600 --lr 0.0004 --kld_coef 0.001 --bind_mode 'fourier'
